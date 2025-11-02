f130a57da269dd264090e79f4d60fcda069523d8
yvan-allioux <yvan.allioux.pro@gmail.com>
2025-11-02 07:35:58 -0800

    Add Docker installation steps for Debian 12

--- 2_apt_docker.sh
+++ 2_apt_docker.sh
@@ -25,3 +25,16 @@ rm lazydocker_*.tar.gz LICENSE README.md
 
 #UBUNTU
 sudo apt-get update;sudo apt-get install ca-certificates curl;sudo install -m 0755 -d /etc/apt/keyrings;sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc;sudo chmod a+r /etc/apt/keyrings/docker.asc;echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu noble stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;sudo apt-get update;sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin;sudo usermod -aG docker $USER;sudo systemctl restart docker;newgrp docker
+
+#debian12
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian bookworm stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker $USER
+sudo systemctl restart docker
+newgrp docker
